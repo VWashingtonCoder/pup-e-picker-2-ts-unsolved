02430a74953f9c09d5130d70c4a0504a44f9ecc5
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-10-05 16:52:09 -0600

    Update form with states and handlers

--- src/Components/CreateDogForm.tsx
+++ src/Components/CreateDogForm.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
 import { dogPictures } from "../dog-pictures";
+import toast from "react-hot-toast";
 
 export const CreateDogForm = () =>
   // no props allowed
   {
     const [selectedImage, setSelectedImage] = useState(dogPictures.BlueHeeler);
+    const [dogName, setDogName] = useState("");
+    const [dogDescription, setDogDescription] = useState("");
+
+    const handleSubmit = () => {
+      const newDog = {
+        name: dogName,
+        description: dogDescription,
+        picture: selectedImage,
+      };
+
+      if(!dogName || !dogDescription) return toast.error("Please fill out all fields");
+      else console.log(newDog);
+    }
+
 
     return (
       <form
@@ -12,13 +27,31 @@ export const CreateDogForm = () =>
         id="create-dog-form"
         onSubmit={(e) => {
           e.preventDefault();
+          handleSubmit();
         }}
       >
         <h4>Create a New Dog</h4>
         <label htmlFor="name">Dog Name</label>
-        <input type="text" />
+        <input 
+          id="name"
+          type="text"
+          value={dogName}
+          onChange={(e) => {
+            setDogName(e.target.value);
+          }} 
+        />
+
         <label htmlFor="description">Dog Description</label>
-        <textarea name="" id="" cols={80} rows={10}></textarea>
+        <textarea  
+          id="description" 
+          cols={80} 
+          rows={10}
+          value={dogDescription}
+          onChange={(e) => {
+            setDogDescription(e.target.value);
+          }}
+        ></textarea>
+        
         <label htmlFor="picture">Select an Image</label>
         <select
           id=""
