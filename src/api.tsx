982ba2f8b3e9bc5a265626a6a08cf990b54ee19f
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-10-05 12:21:16 -0600

    Fill in api functions

--- src/api.tsx
+++ src/api.tsx
@@ -1,22 +1,37 @@
 import { Dog } from "./types";
-const API_URL = "http://localhost:3000";
+const API_URL = "http://localhost:3000/dogs";
 
 const getAllDogs = async () => {
-  // fill out method
-  const response = await fetch(`${API_URL}/dogs`);
-  const dogs = response.json();
-  return dogs;
+  const response = await fetch(API_URL);
+  return response.json();
 };
 
-const postDog = () => {
-  // fill out method
+const postDog = async (newDog: Omit<Dog, "id">) => {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newDog),
+  });
+  return response.status;
 };
-const deleteDogRequest = () => {
-  // fill out method
+const deleteDogRequest = async (id: number) => {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+  });
+  return response.status;
 };
 
-const patchFavoriteForDog = () => {
-  // fill out method
+const patchFavoriteForDog = async (id: number, isFavorite: boolean) => {
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ isFavorite }),
+  });
+  return response.json();
 };
 
 export const Requests = {
